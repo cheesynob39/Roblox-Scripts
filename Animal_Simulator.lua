678e8a76b6218b3f1adfd5799671212987cb2188
cheesynob39 <111374516+cheesynob39@users.noreply.github.com>
2022-08-17 17:50:46 +0100

    Update Animal_Simulator.lua

--- Animal_Simulator.lua
+++ Animal_Simulator.lua
@@ -5,7 +5,7 @@
 for i,v in pairs(game.Workspace.Treasures:GetChildren()) do
 if v.ClassName == "MeshPart" then
 repeat
-    wait(.5)
+    wait(_G.Delay)
 
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
 until nil
